16ec0f985e2b5f617e4bbd8aa5a80073d0cde729
Davipol <davi-liz@hotmail.it>
2026-02-02 18:59:40 +0000

    Add filtering by unit and bed to patients endpoint

--- controllers/patients.controller.js
+++ controllers/patients.controller.js
@@ -1,8 +1,17 @@
-const { fetchPatients } = require("../models/patients.model");
+const {
+  fetchPatients,
+  fetchPatientsByFilters,
+} = require("../models/patients.model");
 
 const getPatients = async (req, res, next) => {
   try {
-    const patients = await fetchPatients();
+    const { unit, bed } = req.query;
+    let patients;
+    if (unit || bed) {
+      patients = await fetchPatientsByFilters(unit, bed);
+    } else {
+      patients = await fetchPatients();
+    }
     res.status(200).send({ patients });
   } catch (err) {
     next(err);
--- models/patients.model.js
+++ models/patients.model.js
@@ -5,4 +5,25 @@ const fetchPatients = async () => {
   return rows;
 };
 
-module.exports = { fetchPatients };
+const fetchPatientsByFilters = async (unit, bed) => {
+  let query = "SELECT * FROM patients";
+  const params = [];
+  const conditions = [];
+
+  if (unit) {
+    conditions.push(`unit_id = $${params.length + 1}`);
+    params.push(unit);
+  }
+  if (bed) {
+    conditions.push(`bed = $${params.length + 1}`);
+    params.push(bed);
+  }
+  if (conditions.length > 0) {
+    query += " WHERE " + conditions.join(" AND ");
+  }
+  query += " ORDER BY bed";
+
+  const { rows } = await db.query(query, params);
+  return rows;
+};
+module.exports = { fetchPatients, fetchPatientsByFilters };
